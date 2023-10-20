ae2e5f2aac49717016a343aaa971329aa40f669b
kikimd <a338707@uach.mx>
2023-10-19 20:53:31 -0600

    agregue codigo para poder hacer funcionar el servidor de la base de datos

--- docs/server.js
+++ docs/server.js
@@ -1,1 +1,11 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  // Maneja las solicitudes aquí
+});
+
+const PORT = 3000; // Puerto en el que se ejecutará el servidor
+
+server.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
