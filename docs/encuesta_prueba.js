711a3d198c3eb087c4096f53a0c8022995d9947e
kikimd <a338707@uach.mx>
2023-10-22 13:08:01 -0600

    pruebaotravez

--- docs/encuesta_prueba.js
+++ docs/encuesta_prueba.js
@@ -7,52 +7,38 @@ document.addEventListener("DOMContentLoaded", function () {
         const nombre = document.getElementById("nombre").value;
         const apellido = document.getElementById("apellido").value;
 
-        // Aquí puedes enviar los datos a tu servidor o hacer lo que necesites con ellos
+        // Aquí puedes enviar los datos al servidor o hacer lo que necesites con ellos
         console.log("Nombre: " + nombre);
         console.log("Apellido: " + apellido);
 
-        //código para enviar datos a la base de datos
-        const sql = require('mssql');
-
-        const config = {
-            user: 'dbadmin',
-            password: 'SC123456789*',
-            server: 'sistemascontrolserver.database.windows.net',
-            database: 'bdanalisiscostos',
-        };
-        
-        document.addEventListener("DOMContentLoaded", function () {
-            const form = document.getElementById("encuestaForm");
-        
-            form.addEventListener("submit", function (event) {
-                event.preventDefault();
-        
-                const nombre = document.getElementById("nombre").value;
-                const apellido = document.getElementById("apellido").value;
-        
-                // Establecer la conexión a la base de datos
-                sql.connect(config, (err) => {
-                    if (err) {
-                        console.error('Error al conectar a la base de datos:', err);
-                        return;
-                    }
-        
-                    // Crear una nueva solicitud
-                    const request = new sql.Request();
-        
-                    // Consulta SQL para insertar los datos en la tabla "Encuesta"
-                    request.query(`INSERT INTO Encuesta_prueba (Nombre, Apellido) VALUES ('${nombre}', '${apellido}')`, (err, result) => {
-                        if (err) {
-                            console.error('Error al insertar datos:', err);
-                        } else {
-                            console.log('Datos insertados con éxito.');
-                        }
-        
-                        // Cerrar la conexión a la base de datos
-                        sql.close();
-                    });
-                });
-            });
-        });
+        // Luego de procesar los datos, puedes enviarlos al servidor a través de una solicitud AJAX
+        enviarDatosAlServidor(nombre, apellido);
     });
 });
+
+function enviarDatosAlServidor(nombre, apellido) {
+    // En lugar de usar mssql en el navegador, aquí se enviarían los datos al servidor para su procesamiento en el lado del servidor.
+    // Puedes usar una solicitud AJAX para enviar los datos a un servidor que tenga acceso a la base de datos.
+
+    // Ejemplo de una solicitud AJAX utilizando Fetch API (puede adaptarse a tu backend):
+    fetch('/guardar_datos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nombre, apellido }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Datos enviados con éxito:', data);
+    })
+    .catch(error => {
+        console.error('Error al enviar datos:', error);
+    });
+}
+//Este código agrega la función enviarDatosAlServidor que utiliza Fetch API para enviar los datos al servidor. Debes asegurarte de que el servidor esté configurado para manejar esta solicitud POST en la ruta /guardar_datos y que la lógica del servidor inserte los datos en la base de datos.
+
+
+
+
+
