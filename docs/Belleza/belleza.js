c08b7cf7016eb0bd809ac15a2993a8d63bbfdc25
kikimd <a338707@uach.mx>
2023-11-12 13:03:58 -0600

    cambios

--- docs/Belleza/belleza.js
+++ docs/Belleza/belleza.js
@@ -2613,7 +2613,7 @@ document.getElementById('peinados').addEventListener('change', function() {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('Enviar').addEventListener('click', function(event) {
+    document.getElementById('bellezaForm').addEventListener('submit', function(event) {
         event.preventDefault();
         // Obtén los valores de los campos específicos de belleza
         const corteCabello = document.getElementById('corteCabello').value;
