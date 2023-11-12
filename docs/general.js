756cec2f4849941f8bd0eef8df8bfc0bb3c60553
kikimd <a338707@uach.mx>
2023-11-12 13:56:39 -0600

    cambios

--- docs/general.js
+++ docs/general.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         if (rubroSeleccionado === 'belleza') {
             window.location.href = 'Belleza/belleza.html';
-            //const nombre = document.getElementById('nombre').value;
-            //const edad = document.getElementById('edad').value;
+            const nombre = document.getElementById('nombre').value;
+            const edad = document.getElementById('edad').value;
             const correo = document.getElementById('correo').value;
             const telefono = document.getElementById('telefono').value;
             const resultados = document.getElementById('resultados').value;
@@ -43,15 +43,14 @@ async function enviarProductoHojaCalculo(nombre, edad, correo, telefono, resulta
     try {
 
         const respuesta = await fetch("https://sheet.best/api/sheets/d3f7e817-4130-4931-a597-c7638d7e0893",{
-        //const respuesta = await fetch('https://sheet.best/api/sheets/d3f7e817-4130-4931-a597-c7638d7e0893', {
             method: 'POST',
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                //"Nombre": nombre,
-                //"Edad": edad,
+                "Nombre": nombre,
+                "Edad": edad,
                 "Correo": correo,
                 "Teléfono": telefono,
                 "Resultados": resultados,
