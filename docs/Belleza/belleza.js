315c0a40e0202fd96294d16d0f44c0c616e3880f
MirellePeralta <a.mirelleperalta@gmail.com>
2023-11-21 21:59:33 -0600

    Te quiero mucho, bay

--- docs/Belleza/belleza.js
+++ docs/Belleza/belleza.js
@@ -1,29 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const secciones = document.querySelectorAll('.seccion');
-
-    secciones.forEach(seccion => {
-        seccion.style.display = 'none';
-    });
-    
-    document.getElementById('rubro').addEventListener('change', function() {
-        const bellezaDiv = document.querySelector('.salud-belleza-bienestar');
-        const textilDiv = document.querySelector('.ropa-zapatos-accesorios');
-
-        if (this.value === 'belleza') {
-            bellezaDiv.style.display = 'block';
-            textilDiv.style.display = 'none';  // Asegúrate de ocultar la otra sección
-        } 
-        else if (this.value === 'textil') {
-            bellezaDiv.style.display = 'none';  // Asegúrate de ocultar la otra sección
-            textilDiv.style.display = 'block';
-        }
-        else if (this.value === 'elegir') {
-            bellezaDiv.style.display = 'none';
-            textilDiv.style.display = 'none';
-        }
-    });
-});
-
 document.getElementById('servicios').addEventListener('change', function() {
     const belleza1Div = document.querySelector('.belleza1');
 
